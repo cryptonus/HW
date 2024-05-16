70c8c15987f94a2c5ad3095757684e52bfdda6da
cryptonus <52319563+cryptonus@users.noreply.github.com>
2024-05-16 18:57:55 +0100

    change

--- obj/Debug/net8.0/HW.AssemblyInfo.cs
+++ obj/Debug/net8.0/HW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6537de64abfc912abe41fb0c945560a972ecb0c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
